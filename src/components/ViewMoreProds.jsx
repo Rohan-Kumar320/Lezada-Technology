e16b8440469cb92703296ee4382e0e3a9337cab4
Rohan-Kumar320 <rohanjodhani@gmail.com>
2024-12-24 14:37:46 +0500

    trigger fix

--- src/components/ViewMoreProds.jsx
+++ src/components/ViewMoreProds.jsx
@@ -3,85 +3,137 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import React, { useEffect, useState } from 'react'
 import SplitType from 'split-type';
 
+gsap.registerPlugin(ScrollTrigger)
+
 const ViewMoreProds = ({trigger}) => {
     const [imgLoaded, setImgLoaded] = useState(false);
 
     const handleImgLoad = () =>{
         setImgLoaded(true)
     }
+    // useEffect(() => {
+
+    //     if(trigger && imgLoaded){
+
+    //         // const splitText = new SplitType('.paraani',{types :'words'})
+    //         // const words = splitText.words
+            
+    //         const parentAni = gsap.from(['.parentani','.textani'],{
+    //             xPercent: -500,
+    //             duration: 1.5,
+    //             scrollTrigger: {
+    //                 trigger: '.parentani',
+    //                 start: 'top 80%',
+    //             }
+    //         })
+
+    //         const textAni = gsap.from('.textani',{
+    //             xPercent: -1000,
+    //             duration: 2,
+    //             scrollTrigger: {
+    //                 trigger: '.parentani',
+    //                 start: 'top 80%',
+    //             }
+    //         })
+
+    //         const imgAni = gsap.from('.imgani',{
+    //             yPercent: 4,
+    //             duration: 1.5,
+    //             yoyoEase: true,
+    //             repeat: -1,
+    //         })
+            
+
+    //         const parentAni2 = gsap.from('.parentani2',{
+    //             xPercent: -500,
+    //             duration: 1.5,
+    //             scrollTrigger: {
+    //                 trigger: '.parentani2',
+    //                 start: 'top 80%',
+    //             }
+    //         })
+
+    //         const textAni2 = gsap.from('.textani2',{
+    //             xPercent: -1000,
+    //             duration: 2,
+    //             scrollTrigger: {
+    //                 trigger: '.parentani2',
+    //                 start: 'top 80%',
+    //             }
+    //         })
+
+    //         const imgAni2 = gsap.from('.imgani2',{
+    //             yPercent: 4,
+    //             duration: 1.5,
+    //             yoyoEase: true,
+    //             repeat: -1,
+    //         })
+            
+    //         ScrollTrigger.refresh();
+            
+    //         return () => {
+    //             parentAni.kill();
+    //             imgAni.kill();
+    //             textAni.kill();
+    //             parentAni2.kill();
+    //             imgAni2.kill();
+    //             textAni2.kill();
+    //             ScrollTrigger.refresh();
+    //         }
+    //     }
+    // }, [trigger, imgLoaded])
     useEffect(() => {
+    if (trigger && imgLoaded) {
+      // Select elements dynamically and animate them independently
+        const parentElements = gsap.utils.toArray('.parentani, .parentani2');
+        const textElements = gsap.utils.toArray('.textani, .textani2');
+        const imgElements = gsap.utils.toArray('.imgani, .imgani2');  
 
-        if(trigger && imgLoaded){
-
-            gsap.registerPlugin(ScrollTrigger)
-            // const splitText = new SplitType('.paraani',{types :'words'})
-            // const words = splitText.words
-            
-            const parentAni = gsap.from('.parentani',{
-                xPercent: -500,
-                duration: 1.5,
-                scrollTrigger: {
-                    trigger: '.parentani',
-                    start: 'top 80%',
-                }
+        const parentAnimations = parentElements.map((el) =>
+            gsap.from(el, {
+            xPercent: -500,
+            duration: 1.5,
+            scrollTrigger: {
+                trigger: el,
+                start: 'top 80%',
+            },
             })
+        );
 
-            const textAni = gsap.from('.textani',{
-                xPercent: -1000,
-                duration: 2,
-                scrollTrigger: {
-                    trigger: '.parentani',
-                    start: 'top 80%',
-                }
+        const textAnimations = textElements.map((el) =>
+            gsap.from(el, {
+            xPercent: -1000,
+            duration: 2,
+            scrollTrigger: {
+                trigger: el.closest('.parentani, .parentani2'),
+                start: 'top 80%',
+            },
             })
+        );
 
-            const imgAni = gsap.from('.imgani',{
-                yPercent: 4,
-                duration: 1.5,
-                yoyoEase: true,
-                repeat: -1,
+        const imgAnimations = imgElements.map((el) =>
+            gsap.from(el, {
+            yPercent: 4,
+            duration: 1.5,
+            yoyoEase: true,
+            repeat: -1,
             })
-            
+        );
 
-            const parentAni2 = gsap.from('.parentani2',{
-                xPercent: -500,
-                duration: 1.5,
-                scrollTrigger: {
-                    trigger: '.parentani2',
-                    start: 'top 80%',
-                }
-            })
+        ScrollTrigger.refresh();
 
-            const textAni2 = gsap.from('.textani2',{
-                xPercent: -1000,
-                duration: 2,
-                scrollTrigger: {
-                    trigger: '.parentani2',
-                    start: 'top 80%',
-                }
-            })
-
-            const imgAni2 = gsap.from('.imgani2',{
-                yPercent: 4,
-                duration: 1.5,
-                yoyoEase: true,
-                repeat: -1,
-            })
-            
-            ScrollTrigger.refresh();
-            
-            return () => {
-                parentAni.kill();
-                imgAni.kill();
-                textAni.kill();
-                parentAni2.kill();
-                imgAni2.kill();
-                textAni2.kill();
-                ScrollTrigger.refresh();
-            }
+        return () => {
+        // Kill all animations and refresh
+        [...parentAnimations, ...textAnimations, ...imgAnimations].forEach((ani) => ani.kill());
+        ScrollTrigger.refresh();
+        };
         }
-    }, [trigger, imgLoaded])
+    }, [trigger, imgLoaded]);
     
+
+
+
+
     return (
     <>
             <div id='viewMore' className='parentani max-w-full max-h-full bg-gray-300 mb-6 m-4'>
@@ -89,7 +141,7 @@ const ViewMoreProds = ({trigger}) => {
                     <div className='flex flex-col justify-center items-center md:items-start overflow-hidden'>
                         <h1 className='textani text-lg sm:text-3xl md:text-4xl'><span className='font-bold'>07+</span> Shop</h1>
                         <h1 className='textani text-lg sm:text-3xl md:text-4xl'>laptop design's</h1>
-                        <p className='textani pt-2 w-40 md:w-60 font-normal md:text-sm text-xs md:text-start text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laudantium assumenda molestiae quisquam doloremque architecto.</p>
+                        <p className='textani pt-2 w-45 md:w-60 font-normal md:text-sm text-xs md:text-start text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laudantium assumenda molestiae quisquam doloremque architecto.</p>
                         <button className='textani mb-3 mt-3 border-b-2 border-black hover:animate-pulse active:text-blue-500'>View More</button>
                     </div>
                     <div className='imgani max-w-[50%] md:max-w-[32%]'>
